b2196f474a2b55b446bd3aaf3c27699d9bc9bf71
Alvaro Alexander Palacios Carrillo <79853359+apalacioscarrill@users.noreply.github.com>
2022-01-22 14:57:52 -0500

    ADD: Franchises enum to contact model

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,3 +1,25 @@
 class Contact < ApplicationRecord
+  CREDIT_CARD_FRANCHISES = %i[
+    amex
+    unionpay
+    dankort
+    diners
+    elo
+    discover
+    hipercard
+    jcb
+    maestro
+    mastercard
+    mir
+    rupay
+    solo
+    switch
+    visa
+  ].freeze
+
+  # Relations
   belongs_to :user
+
+  # Enums
+  enum franchise: CREDIT_CARD_FRANCHISES
 end
