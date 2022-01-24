38051b591a54da57dacbb88c0b780028c28486b6
Alvaro Alexander Palacios Carrillo <79853359+apalacioscarrill@users.noreply.github.com>
2022-01-24 01:36:08 -0500

    UPD: seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,1 +1,11 @@
 User.create(email: 'user_1@gmail.com', password: 'securepassword')
+second_user = User.create(email: 'user_2@gmail.com', password: 'securepassword')
+
+second_user.contacts.create(
+  name: 'Test Name',
+  phone: '(+57) 320-432-05-09',
+  address: 'Test Address',
+  credit_card: '371449635398431',
+  franchise: 'amex',
+  email: 'adrian@gmail.com'
+)
