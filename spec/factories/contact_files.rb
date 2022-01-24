9705b3bf4061ccf6889cd85b2eaebc75a108c539
Alvaro Alexander Palacios Carrillo <79853359+apalacioscarrill@users.noreply.github.com>
2022-01-24 00:08:26 -0500

    ADD: ContactFile model spec

--- spec/factories/contact_files.rb
+++ spec/factories/contact_files.rb
@@ -1,7 +1,6 @@
 FactoryBot.define do
   factory :contact_file do
     association :user
-    status { rand(0..3) }
     file { Rack::Test::UploadedFile.new(Rails.root.join('spec', 'factories', 'files', 'example.csv')) }
   end
 end
---
+++ spec/models/contact_file_spec.rb
@@ -0,0 +1,20 @@
+require 'rails_helper'
+
+RSpec.describe ContactFile, type: :model do
+  include ActiveJob::TestHelper
+  after { clear_enqueued_jobs }
+
+  context 'associations' do
+    it { should belong_to(:user) }
+    it { should define_enum_for(:status).with_values(%i[on_hold processing failed terminated]) }
+  end
+
+  context 'create' do
+    it 'must enqueue ImportContactsJob and create contact_file in status on_hold' do
+      contract_file = create(:contact_file)
+
+      expect(contract_file.on_hold?).to eq(true)
+      expect(enqueued_jobs.detect { |a| a['job_class'] == ImportContactsJob.to_s }).not_to be_nil
+    end
+  end
+end
