117065fcc6f50dde48ec31f76c86b45a5ca0d2cb
Alvaro Alexander Palacios Carrillo <79853359+apalacioscarrill@users.noreply.github.com>
2022-01-23 23:40:08 -0500

    FIX: file in ContactFile factory

--- spec/factories/contact_files.rb
+++ spec/factories/contact_files.rb
@@ -2,13 +2,6 @@ FactoryBot.define do
   factory :contact_file do
     association :user
     status { rand(0..3) }
-
-    after(:build) do |contact_file|
-      contact_file.file.attach(
-        io: File.open(Rails.root.join('spec', 'factories', 'files', 'example.csv')),
-        filename: 'example.csv',
-        content_type: 'text/csv'
-      )
-    end
+    file { Rack::Test::UploadedFile.new(Rails.root.join('spec', 'factories', 'files', 'example.csv')) }
   end
 end
